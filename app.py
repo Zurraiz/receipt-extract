5bb8f2a8d0c9fc7412a93226178d04599d9ab673
PMLS <pmls@example.com>
2026-04-23 19:24:17 +0500

    Update runtime port to 8080 for deployment

--- app.py
+++ app.py
@@ -128,5 +128,5 @@ def export_csv():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8080))
     serve(app, host="0.0.0.0", port=port)
